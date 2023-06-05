2a8b44f27a0b15f79358f6af9ea2ebdc7a1a9fc8
Shuvroprintbaz <shuvro.printbaz@gmail.com>
2023-06-05 10:48:15 +0600

    edited time

--- src/Components/orderList/OrderList.jsx
+++ src/Components/orderList/OrderList.jsx
@@ -192,7 +192,7 @@ let matchingMerchant
                     </div>
                     <div className="col-lg-1 col-sm-12">
                       <p className="status-btn">{orders?.orderStatus}</p>
-                      <p style={{fontSize: '14px'}}>{orders?.createdAt}</p>
+                      <p style={{fontSize: '14px'}}>{orders?.createdAt?.slice(0,10)}</p>
                     </div>
                   </div>
                 </Link>
