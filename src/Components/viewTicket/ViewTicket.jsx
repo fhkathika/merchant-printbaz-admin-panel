52fdc75f684bf68a32985094d565907313b1f987
Shuvroprintbaz <shuvro.printbaz@gmail.com>
2023-06-19 18:10:39 +0600

    push

--- src/Components/viewTicket/ViewTicket.jsx
+++ src/Components/viewTicket/ViewTicket.jsx
@@ -429,7 +429,7 @@ const ViewTicket = () => {
                    
                      multiple
                      
-                    //  onChange={handleFileChange}
+                   onChange={handleFileChange}
                    />
                    </button>
 
