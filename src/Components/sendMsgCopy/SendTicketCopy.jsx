f6d45774d990dce2aa4ebfb46057770b5762a5c2
Shuvroprintbaz <shuvro.printbaz@gmail.com>
2023-07-16 16:53:08 +0600

    fixed email sent issue

--- src/Components/sendMsgCopy/SendTicketCopy.jsx
+++ src/Components/sendMsgCopy/SendTicketCopy.jsx
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const SendTicketCopy = (ticketCopy) => {
-  // axios.post('https://mserver.printbaz.com/sendTicketCopy', ticketCopy )
-  axios.post('http://localhost:5000/sendTicketCopy', ticketCopy )
+  axios.post('https://mserver.printbaz.com/sendTicketCopy', ticketCopy )
+  // axios.post('http://localhost:5000/sendTicketCopy', ticketCopy )
     .then((response) => {
       console.log("send ticket mail",response);
     })
--- src/Components/sendOrderStatusMail/SendOrderStatusMail.jsx
+++ src/Components/sendOrderStatusMail/SendOrderStatusMail.jsx
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const SendOrderStatusMail = (stausMail) => {
-  // axios.post('https://mserver.printbaz.com/OrderConfirmEmail', stausMail )
-  axios.post('http://localhost:5000/OrderConfirmEmail', stausMail )
+  axios.post('https://mserver.printbaz.com/OrderConfirmEmail', stausMail )
+  // axios.post('http://localhost:5000/OrderConfirmEmail', stausMail )
     .then((response) => {
       console.log("send order status mail",response);
     })
